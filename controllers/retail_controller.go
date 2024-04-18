763bcdfde0b19103415b82181c46b9089ecce6b8
Bagus Perwira <71205584+zdev2@users.noreply.github.com>
2024-04-18 19:43:19 +0800

    Co-authored-by: rakryan.teja@gmail.com <rakryan.teja@gmail.com>

---
+++ controllers/retail_controller.go
@@ -0,0 +1,55 @@
+package controllers
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func RouteRetail(app *fiber.App) {
+	retailGroup := app.Group("/toko")
+	// retailGroup.Get("/barang", GetBarang)
+	// retailGroup.Get("/barang/:id", GetBarangByID)
+	retailGroup.Post("/barang", CreateBarang)
+	// retailGroup.Put("/barang/:id", UpdateBarang)
+	// retailGroup.Put("/barang/stok/:id")
+	// retailGroup.Delete("/barang/:id",)
+}
+
+func CreateBarang(c *fiber.Ctx) error {
+	type AddBarangReq struct {
+		Kode       string `json:"kode_barang"`
+		Nama       string `json:"nama_barang"`
+		HargaPokok int    `json:"harga_pokok"`
+		HargaJual  int    `json:"harga_jual"`
+		Tipe       string `json:"tipe_barang"`
+		Stok       int    `json:"stok"`
+	}
+	req := new(AddBarangReq)
+
+	if err := c.BodyParser(req); err != nil {
+		return c.Status(fiber.StatusBadRequest).
+			JSON(map[string]any{
+				"message": "Body Not Valid",
+			})
+
+	}
+	// isValid && err != nil {
+	//   return c.Status(fiber.StatusBadRequest).
+	//   JSON(map[string]any{
+	//     "message": err.Error(),
+	//   })
+	//   errCreateBarang := utils.CreateBarang(model.Barang{})
+
+	// if errCreateBarang != nil {
+	//   logrus.Printf("Terjadi error : &s\n", errCreateBarang.Error())
+	//   return c.Status(fiber.StatusInternalServerError).
+	//   JSON(map[string]any{
+	//     "message": "Server Error",
+	//   })
+
+	// }
+	return c.Status(fiber.StatusOK).
+		JSON(map[string]any{
+			"message": "Berhasil Menambahkan Barang",
+			"Barang":  "jawa",
+		})
+}
--- model/base.go
+++ model/base.go
@@ -1,10 +1,14 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Model struct {
-  ID uint `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	DeletedAt time.Time `json:"deleted_at"`
+	ID        uint           `gorm:"primarykey" json:"id"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
 }
